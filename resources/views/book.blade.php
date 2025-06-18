c7e2048b8fa9e3161fbe8dd3baf3fa4f03d21801
stbnrivas <stbnrivas@gmail.com>
2025-06-18 19:32:47 +0200

    set color red to error at booking

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -14,10 +14,10 @@
         </div>
     </div>
     @else
-    <div class="col-span-full bg-emerald-500  mx-auto min-w-full h-120 text-white font-bold pt-15">
+    <div class="col-span-full bg-red-900  mx-auto min-w-full h-120 text-red-200 font-bold pt-15">
         <span class="block text-center text-8xl font-roboto">{{ __('Sorry, something was wrong') }}</span>
         <div class="block pt-15">
-            <a href="/{{ app()->getLocale() }}/" class="block mx-auto underline text-center text-2xl font-poppins bg-white text-emerald-500 p-3 w-md rounded-xl">{{ __('return back to index') }}</a>
+            <a href="/{{ app()->getLocale() }}/" class="block mx-auto underline text-center text-2xl font-poppins bg-white text-red-500 p-3 w-md rounded-xl">{{ __('return back to index') }}</a>
         </div>
     </div>
     @endif
